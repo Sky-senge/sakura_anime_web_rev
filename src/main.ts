a92881e824decd6fc6f169923662567bcac64700
Radium-bit <Radiumbit@protonmail.com>
2024-11-22 11:03:55 +0800

    Project Init
    初始化并添加了ElementPlus和ArcoDesign组件

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPersistedstate from 'pinia-plugin-persistedstate'
+
+import App from './App.vue'
+import router from './router'
+
+import ElementPlus from 'element-plus'
+import ArcoVue from '@arco-design/web-vue';
+import 'element-plus/dist/index.css'
+import '@arco-design/web-vue/dist/arco.css';
+
+
+const app = createApp(App)
+const pinia = createPinia()
+pinia.use(piniaPersistedstate)
+app.use(pinia)
+app.use(router)
+app.use(ElementPlus)
+app.use(ArcoVue)
+
+app.mount('#app')
