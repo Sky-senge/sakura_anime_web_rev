89a9aae47fe6217a2b043bc7ab4b0b023777af99
Lunk <3452094168@qq.com>
2024-11-25 17:57:36 +0800

    下载了Axios，新建了Home页面
    因为太久没写一下午在重新复习Vue框架，web进度+0.1 QAQ

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,14 @@
 // @ts-ignore
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Home />
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -83,4 +71,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
