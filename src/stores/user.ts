742958e7c78df4e1aa997f58aa615d42e861b06d
Radium-bit <Radiumbit@protonmail.com>
2024-12-13 11:50:02 +0800

    在UserStorege中存储管理员状态
    在UserStorege中存储管理员状态，以优化重复请求数量

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -4,23 +4,28 @@ export const useUserStore = defineStore('user', {
   state: () => ({
     token: '',
     userId: null as number | null,
+    isAdmin: false as boolean
   }),
   actions: {
-    setUser({ token, userId }: { token: string; userId: number }) {
+    setUser({ token, userId, isAdmin }: { token: string; userId: number; isAdmin: boolean}) {
       this.token = token
       this.userId = userId
+      this.isAdmin = isAdmin
       localStorage.setItem('token', token)
       localStorage.setItem('userId', userId.toString())
+      localStorage.setItem('aXNBZG1pbj8=', isAdmin ? '1' : '0')
     },
     loadUser() {
       this.token = localStorage.getItem('token') || ''
       this.userId = Number(localStorage.getItem('userId')) || null
+      this.isAdmin = Boolean(Number(localStorage.getItem('aXNBZG1pbj8='))===1) || false
     },
     clearUser() {
       this.token = ''
       this.userId = null
       localStorage.removeItem('token')
       localStorage.removeItem('userId')
+      localStorage.setItem('aXNBZG1pbj8=','0')
     },
   },
 })
