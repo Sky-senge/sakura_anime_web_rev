ef9975bdb41d2e38cb13cdd628e61003b119baf3
Radium-bit <Radiumbit@protonmail.com>
2024-12-23 15:24:35 +0800

    实现Request库跟随全局URL

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,15 +1,19 @@
 import axios from 'axios';
 import { useUserStore } from '@/stores/user'; 
+import { useGlobalStore } from '@/stores/globalSettings';
 
 //API 请求
 const request = axios.create({
-  baseURL: 'http://127.0.0.1:8080/api',
+  baseURL: 'NullURL',
   timeout: 5000,
 });
 
 // 请求拦截器：添加 Token 到请求头
 request.interceptors.request.use(
   (config) => {
+    const globalSettings = useGlobalStore(); // 从全局设置加载后端URL
+    const serverUrl = globalSettings.serverUrl;
+    config.baseURL = `${serverUrl}/api`
     const userStore = useUserStore(); // 获取 Pinia 的 user store
     userStore.loadUser(); //命令userStore加载用户
     const token = userStore.token || localStorage.getItem('token'); // 获取 token
@@ -25,7 +29,7 @@ request.interceptors.request.use(
 
 //文件请求
 const fileRequest = axios.create({
-  baseURL: 'http://127.0.0.1:8080/files', // 文件路径的根路径
+  baseURL: 'NullURL', // 文件路径的根路径
   timeout: 0, //因为暂时没写流响应，只能停用超时的方式进行上传
 });
 //添加 Token 到文件路径
@@ -33,6 +37,9 @@ fileRequest.interceptors.request.use(
   (config) => {
     const userStore = useUserStore();
     userStore.loadUser(); //命令userStore加载用户
+    const globalSettings = useGlobalStore(); // 从全局设置加载后端URL
+    const serverUrl = globalSettings.serverUrl;
+    config.baseURL = `${serverUrl}/files`
     const token = userStore.token || localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`; // 设置 Token 到 Authorization 头
